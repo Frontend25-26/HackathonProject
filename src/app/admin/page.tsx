aa769cb6daef8043d1f45c92ed3ebfcc587fedfb
Bespalov_Aleksandr <sonkot25@mail.ru>
2026-03-28 16:28:08 +0300

    feat: feat 22 save progress

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,21 @@
+import {FC} from "react";
+
+interface AdminProps {
+    id: number;
+    name?: string;
+}
+
+const Page:FC<AdminProps> = (props) => {
+    const propName = props.name;
+    return (
+        <div>
+            <main>
+                <h1>
+                    Hello, admin {propName}!
+                </h1>
+            </main>
+        </div>
+    );
+}
+
+export default Page;
---
+++ src/app/student/page.tsx
@@ -0,0 +1,22 @@
+import {FC} from "react";
+
+interface StudentProps {
+    id: number;
+    name?: string;
+}
+
+const Page:FC<StudentProps> = (props) => {
+    const propName = props.name;
+    return (
+        <div>
+            <main>
+                <h1>
+                    Hello, student {propName}!
+                </h1>
+            </main>
+        </div>
+    );
+}
+
+export default Page;
+
