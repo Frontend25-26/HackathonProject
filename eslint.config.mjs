6b9e28a0d3881019c3399fb2d0f952ab36449e5f
alexwolf120 <amitropolov@gmail.com>
2026-04-21 11:36:53 +0300

    feat: folders and eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'eslint/config'
 import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
 import importPlugins from 'eslint-plugin-import'
 import tseslint from 'typescript-eslint'
+import boundaries from 'eslint-plugin-boundaries'
 
 import eslintignore from './eslintignore.mjs'
 
@@ -13,6 +14,7 @@ export default defineConfig([
         files: ['**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts,mdx}'],
         plugins: {
             import: importPlugins,
+            boundaries: boundaries,
         },
         rules: {
             semi: ['error', 'never'],
@@ -38,14 +40,28 @@ export default defineConfig([
             ],
             'import/newline-after-import': ['error', { count: 1 }],
             'sort-imports': 'off',
-            'no-restricted-imports': ['error', {
-                patterns: ['**/app/**', '**/pages/**', '**/processes/**', '**/widgets/**',]
+            'boundaries/element-types': [2, {
+                default: 'disallow',
+                rules: [
+                    { from: 'app', allow: ['widgets', 'features', 'entities', 'shared'] },
+                    { from: 'widgets', allow: ['features', 'entities', 'shared'] },
+                    { from: 'features', allow: ['entities', 'shared'] },
+                    { from: 'entities', allow: ['shared'] },
+                    { from: 'shared', allow: [] },
+                ]
             }],
         },
         settings: {
             'import/resolver': {
                 typescript: true,
             },
+            'boundaries/elements': [
+                { type: 'app', pattern: 'app/*', mode: 'full' },
+                { type: 'widgets', pattern: 'widgets/*', mode: 'full' },
+                { type: 'features', pattern: 'features/*', mode: 'full' },
+                { type: 'entities', pattern: 'entities/*', mode: 'full' },
+                { type: 'shared', pattern: 'shared/*', mode: 'full' },
+            ],
         },
     },
     {
