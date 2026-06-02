766ad36d723d87e8c8d8703c02267bfb0a1690c0
alexwolf120 <amitropolov@gmail.com>
2026-06-02 18:50:57 +0300

    feat/84: debug index.ts

--- src/entities/assignment/index.ts
+++ src/entities/assignment/index.ts
@@ -1,2 +1,3 @@
 export { StudentAssignments } from './ui/StudentAssignments';
-export type { Assignment, AssignmentStatus } from './api/types';
+export type { Assignment } from '@/shared/types/assignment';
+export { AssignmentStatus } from '@/shared/types/assignment';
