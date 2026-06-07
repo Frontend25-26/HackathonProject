bb1cb484f834e516811e88c8c50354092f1462d2
alexwolf120 <amitropolov@gmail.com>
2026-06-07 23:30:48 +0300

    feat/87: debug index submission

--- src/entities/submission/index.ts
+++ src/entities/submission/index.ts
@@ -2,3 +2,4 @@ export type { DisplayedSubmission } from './types';
 export { MyWork } from './ui/MyWork';
 export { CommitHistory } from './ui/CommitHistory';
 export { CiBadge } from './ui/CiBadge';
+export type { Submission } from '@/shared/types/submission';
