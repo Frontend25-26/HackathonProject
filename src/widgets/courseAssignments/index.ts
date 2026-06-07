89a43e72c5c7ad28abec86efb108c7f46ae8706e
nastya <nastyaka262003@gmail.com>
2026-06-08 02:22:39 +0300

    feat: страница курса с заголовком курса и списком ДЗ

---
+++ src/widgets/courseAssignments/index.ts
@@ -0,0 +1,1 @@
+export { CourseAssignmentsWidget } from './CourseAssignmentsWidget';
---
+++ src/widgets/courseAssignments/utils/types.ts
@@ -0,0 +1,3 @@
+import { Assignment } from '@/shared/types/assignment';
+
+export type AssignmentRow = Assignment & { index: number };
